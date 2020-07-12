42c69c9aa14150f0eb97a95158484b24fffb3abd
Jonas Tomanga <celleb@mrcelleb.com>
2020-07-12 11:10:54 +0200

    Personal modifications

--- server.ts
+++ server.ts
@@ -1,4 +1,3 @@
-import 'generate-it-logger';
 import run from '@/cli/run';
 import app from './src/app';
 import appCli from './src/app.cli';
@@ -9,17 +8,20 @@ const cliInput = appCli();
 const PORT = cliInput.port || config.port;
 
 app()
-  .then((expressApp) => {
-    if (cliInput['run-script']) {
-      // call the script runner
-      run(cliInput['run-script']);
-    } else {
-      // Start listening for incoming HTTP requests
-      expressApp.listen(PORT, () => {
-        console.log(`${packageJson.name}:${packageJson.version} server listening on port, ${PORT}`);
-      });
-    }
-  }).catch((e) => {
-  console.error('Error calling the app:');
-  throw e;
-});
+    .then((expressApp) => {
+        if (cliInput['run-script']) {
+            // call the script runner
+            run(cliInput['run-script']);
+        } else {
+            // Start listening for incoming HTTP requests
+            expressApp.listen(PORT, () => {
+                console.log(
+                    `${packageJson.name}:${packageJson.version} server listening on port, ${PORT}`
+                );
+            });
+        }
+    })
+    .catch((e) => {
+        console.error('Error calling the app:');
+        throw e;
+    });
