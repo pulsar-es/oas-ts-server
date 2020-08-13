c20706198e604488421a2ea701878cb3b123756b
Jonas Tomanga <celleb@mrcelleb.com>
2020-08-13 20:18:11 +0200

    allow error body

--- src/http/nodegen/middleware/handle401.ts
+++ src/http/nodegen/middleware/handle401.ts
@@ -1,7 +1,8 @@
 import express = require('express');
-import http401 from '../errors/401'
+import http401 from '../errors/401';
+import _ from 'lodash';
 
-import NodegenRequest from '../../interfaces/NodegenRequest'
+import NodegenRequest from '../../interfaces/NodegenRequest';
 
 /**
  * Required for if an unauthorised response should be thrown from a domain or controller
@@ -9,11 +10,15 @@ import NodegenRequest from '../../interfaces/NodegenRequest'
  * @returns {Function}
  */
 export default () => {
-  return (err: any, req: NodegenRequest, res: express.Response, next: express.NextFunction) => {
-    if (err instanceof http401) {
-      res.status(401).send()
-    } else {
-      next(err)
-    }
-  }
-}
+    return (err: any, req: NodegenRequest, res: express.Response, next: express.NextFunction) => {
+        if (err instanceof http401) {
+            if (_.isObject(err.message)) {
+                res.status(403).json(err.message);
+            } else {
+                res.status(401).send();
+            }
+        } else {
+            next(err);
+        }
+    };
+};
--- src/http/nodegen/middleware/handle403.ts
+++ src/http/nodegen/middleware/handle403.ts
@@ -1,5 +1,6 @@
 import express = require('express');
 import http403 from '../errors/403';
+import _ from 'lodash';
 
 import NodegenRequest from '../../interfaces/NodegenRequest';
 
@@ -9,11 +10,15 @@ import NodegenRequest from '../../interfaces/NodegenRequest';
  * @returns {Function}
  */
 export default () => {
-  return (err: any, req: NodegenRequest, res: express.Response, next: express.NextFunction) => {
-    if (err instanceof http403) {
-      res.status(403).send()
-    } else {
-      next(err)
-    }
-  }
-}
+    return (err: any, req: NodegenRequest, res: express.Response, next: express.NextFunction) => {
+        if (err instanceof http403) {
+            if (_.isObject(err.message)) {
+                res.status(403).json(err.message);
+            } else {
+                res.status(403).send();
+            }
+        } else {
+            next(err);
+        }
+    };
+};
